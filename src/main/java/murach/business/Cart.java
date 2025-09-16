b20331bfc41493a192eb5d86aa3c40f652237fbc
Quan-Man <giaman.140904@gmail.com>
2025-09-16 11:30:30 +0700

    upload

---
+++ src/main/java/murach/business/Cart.java
@@ -0,0 +1,63 @@
+package murach.business;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Cart implements Serializable {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private ArrayList<LineItem> items;
+
+    public Cart() {
+        items = new ArrayList<LineItem>();
+    }
+
+    public ArrayList<LineItem> getItems() {
+        return items;
+    }
+
+    public int getCount() {
+        return items.size();
+    }
+
+    public void addItem(LineItem item) {
+        String code = item.getProduct().getCode();
+        int quantity = item.getQuantity();
+        for (LineItem cartItem : items) {
+        	if (cartItem.getProduct().getCode().equals(code)) {
+                cartItem.setQuantity(cartItem.getQuantity() + quantity);
+                return;
+            }
+        }
+        items.add(item);
+    }
+
+    public void updateItem(LineItem item) {
+        String code = item.getProduct().getCode();
+        int quantity = item.getQuantity();
+        for (LineItem cartItem : items) {
+            if (cartItem.getProduct().getCode().equals(code)) {
+                if (quantity > 0) {
+                    cartItem.setQuantity(quantity);
+                } else {
+                    items.remove(cartItem);
+                }
+                return;
+            }
+        }
+    }
+
+    public void removeItem(LineItem item) {
+        String code = item.getProduct().getCode();
+        for (int i = 0; i < items.size(); i++) {
+            LineItem lineItem = items.get(i);
+            if (lineItem.getProduct().getCode().equals(code)) {
+                items.remove(i);
+                return;
+            }
+        }
+    }
+}
